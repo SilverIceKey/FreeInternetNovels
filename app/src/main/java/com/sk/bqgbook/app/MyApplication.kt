6ad3a9fa243cd145aef185511fe438eea4f203d6
SilverKnife <z516798599@qq.com>
2019-05-26 17:54:50 +0800

    使用过程中发现出现闪退，又不好一直连着收集，故集成bugly进行收集

--- app/src/main/java/com/sk/bqgbook/app/MyApplication.kt
+++ app/src/main/java/com/sk/bqgbook/app/MyApplication.kt
@@ -1,16 +1,19 @@
 package com.sk.bqgbook.app
 
 import android.app.Application
+import androidx.multidex.MultiDex
+import androidx.multidex.MultiDexApplication
 import com.blankj.utilcode.util.Utils
 import com.scwang.smartrefresh.layout.SmartRefreshLayout
 import com.scwang.smartrefresh.layout.footer.ClassicsFooter
 import com.scwang.smartrefresh.layout.header.ClassicsHeader
 import com.silvericekey.skutilslibrary.NetUtils.HttpUtils
 import com.sk.bqgbook.R
+import com.tencent.bugly.crashreport.CrashReport
 import io.realm.Realm
 import io.realm.RealmConfiguration
 
-class MyApplication : Application() {
+class MyApplication : MultiDexApplication() {
     companion object {
         var instance: MyApplication? = null
 
@@ -18,7 +21,7 @@ class MyApplication : Application() {
             SmartRefreshLayout.setDefaultRefreshHeaderCreator { context, layout ->
                 layout.setPrimaryColorsId(R.color.white,R.color.colorPrimary)
                 return@setDefaultRefreshHeaderCreator ClassicsHeader(context)
-            }
+            } 
             SmartRefreshLayout.setDefaultRefreshFooterCreator { context, layout ->
                 return@setDefaultRefreshFooterCreator ClassicsFooter(context).setDrawableSize(20f)
             }
@@ -30,6 +33,7 @@ class MyApplication : Application() {
         instance = this
         Utils.init(this)
         HttpUtils.setBaseUrl(CommonParams.base_url)
+        CrashReport.initCrashReport(getApplicationContext(), "306e37954d", false);
         initRealm()
     }
 
